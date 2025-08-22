2ab1b6f07f84a5997cc83ee347a6716290667a5a
Mohammed Musharraf <86569424+mhdmusharraf@users.noreply.github.com>
2025-08-22 17:33:59 +0530

    Small assignment uploaded

---
+++ Univariate.py
@@ -0,0 +1,10 @@
+class Univariate():
+    def quanQual(dataset):
+        quan = []
+        qual = []
+        for columnName in dataset.columns:
+            if(dataset[columnName].dtypes == 'O'):
+                qual.append(columnName)
+            else:
+                quan.append(columnName)
+        return quan,qual
